a9af08058232fef085201a46ad1e7966ad07466e
Ollie0618 <oliverheinzt@gmail.com>
2025-03-19 12:35:19 +0100

    test commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from price_list import PriceList
 shopping_list = ShoppingList()
 shopping_list.add_item("Apples")
 shopping_list.add_item("Bananas")
+shopping_list.add_item("Bananas")
 print(shopping_list.get_items())
 shopping_list.remove_item("Apples")
 print(shopping_list.get_items())
